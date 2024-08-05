db4d2775cbf8a4d3919eaf311d18a0d8e0506c07
Willy Arrojas <willyarrojas@gmail.com>
2024-08-05 16:46:41 +0200

    rename job name

--- 01-jenkins/12-JenkinsJobDSL/parametrizadoDSL.groovy
+++ 01-jenkins/12-JenkinsJobDSL/parametrizadoDSL.groovy
@@ -1,4 +1,4 @@
-job('ejemplo-job-DSL') {
+job('ejemplo-job-DSL-from-GITHUB') {
   description('Job DSL de ejemplo para el curso de Jenkins')
   scm {
     git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
